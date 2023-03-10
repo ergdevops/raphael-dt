ddcf644a18d851f7967785c39f5b3d40cadf68e0
ergdevops <honor6firmware@gmail.com>
2023-03-10 10:34:42 +0530

    added prebuilt kernel

--- device.mk
+++ device.mk
@@ -52,6 +52,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-spiceos
 
+# Kernel
+LOCAL_KERNEL := /home/erg/Desktop/spiceos/kernel/xiaomi/raphael/Zip-out/Image.gz-dtb
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
